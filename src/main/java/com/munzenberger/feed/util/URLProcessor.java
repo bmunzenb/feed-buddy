8dcb5e1e5a34416d7761bebe36dbf5491c0ba8a9
Brian Munzenberger <brian@munzenberger.com>
2016-10-18 17:35:08 -0400

    manually follow redirects

--- src/main/java/com/munzenberger/feed/util/URLProcessor.java
+++ src/main/java/com/munzenberger/feed/util/URLProcessor.java
@@ -12,25 +12,38 @@ public final class URLProcessor {
 	private URLProcessor() {}
 
 	public static InputStream getInputStream(URL url) throws IOException {
-		
-		HttpURLConnection.setFollowRedirects(true);
-		
+
 		URLConnection conn = url.openConnection();
-		
 		return getInputStream(conn);
 	}
-	
+
 	private static InputStream getInputStream(URLConnection conn) throws IOException {
-		
+
+		// handle redirects
+		if (conn instanceof HttpURLConnection) {
+			int response = ((HttpURLConnection)conn).getResponseCode();
+			if (response == HttpURLConnection.HTTP_MOVED_PERM ||
+					response == HttpURLConnection.HTTP_MOVED_TEMP ||
+					response == HttpURLConnection.HTTP_SEE_OTHER ||
+					response == 307 ||
+					response == 308) {
+
+				String location = conn.getHeaderField("Location");
+				if (location != null) {
+					return getInputStream(new URL(location));
+				}
+			}
+		}
+
 		String contentEncoding = conn.getHeaderField("Content-Encoding");
 
 		InputStream in = conn.getInputStream();
-		
+
 		// handle gzip encoded responses
 		if ("gzip".equalsIgnoreCase(contentEncoding)) {
 			in = new GZIPInputStream(in);
 		}
-		
+
 		return in;
 	}
 }
