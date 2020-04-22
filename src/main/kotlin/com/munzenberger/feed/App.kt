ad6891be32f03372ff30075f13ede11063f15b19
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2020-04-22 10:42:19 -0400

    add version information

--- src/main/kotlin/com/munzenberger/feed/App.kt
+++ src/main/kotlin/com/munzenberger/feed/App.kt
@@ -1,5 +1,13 @@
 package com.munzenberger.feed
 
+import java.util.Properties
+
 fun main(args: Array<String>) {
 
+    val version = Properties().apply {
+        val inStream = ClassLoader.getSystemResourceAsStream("version.properties")
+        load(inStream)
+    }
+
+    println("Feed Buddy version ${version["version"]} (https://github.com/bmunzenb/feed-buddy)")
 }
