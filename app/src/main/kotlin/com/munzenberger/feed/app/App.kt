59eb8fc12c62ec92fe30e630dabf986f8985d498
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2024-08-25 23:57:31 -0400

    remove todo comment

--- app/src/main/kotlin/com/munzenberger/feed/app/App.kt
+++ app/src/main/kotlin/com/munzenberger/feed/app/App.kt
@@ -25,7 +25,6 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.Properties
 
-// TODO Move App into a separate gradle module
 fun main(args: Array<String>) {
 
     val versionProperties = Properties().apply {
