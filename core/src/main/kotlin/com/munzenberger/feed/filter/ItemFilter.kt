0538941f139a320793d68735425927be18b68a82
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2025-01-09 16:16:22 -0500

    update dependencies; add plus operator to item processors

--- core/src/main/kotlin/com/munzenberger/feed/filter/ItemFilter.kt
+++ core/src/main/kotlin/com/munzenberger/feed/filter/ItemFilter.kt
@@ -7,4 +7,12 @@ import com.munzenberger.feed.engine.ItemProcessor
 
 interface ItemFilter : ItemProcessor {
     fun evaluate(context: FeedContext, item: Item, logger: Logger): Boolean
+
+    operator fun plus(other: ItemFilter): ItemFilter = CompositeItemFilter(listOf(this, other))
+}
+
+class CompositeItemFilter(private val filters: List<ItemFilter>) : ItemFilter {
+    override fun evaluate(context: FeedContext, item: Item, logger: Logger): Boolean {
+        return filters.all { it.evaluate(context, item, logger) }
+    }
 }
