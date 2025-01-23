6f6da15bf969ac1fab07b81213cfdd30f1ec3bf1
Brian Munzenberger <bmunzenb@users.noreply.github.com>
2025-01-22 23:52:38 -0500

    add versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,26 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("feed-buddy.kotlin-conventions")
+    alias(libs.plugins.versions)
 }
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
+}
+
+// https://github.com/ben-manes/gradle-versions-plugin
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
